5d761996ce8297014018790ea64061ef4b84e922
Ashley Schauer <aelschauer@gmail.com>
2017-02-12 16:27:22 -0700

    fixes migration conflicts after merging contacts and comments functionalities into master. updates seed file to add  comments and contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,10 @@ require 'faker'
 Company.destroy_all
 Job.destroy_all
 Category.destroy_all
+Contact.destroy_all
+Comment.destroy_all
 
-COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
+COMPANIES = ["ESPN", "Aetna", "United Airlines", "YNAB", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = [ "Statistician", "Business Analyst", "Writer", "Accountant", "Architect", "Designer", "Interpreter", "Librarian" "Engineer", "Developer", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
 
@@ -24,3 +26,31 @@ COMPANIES.each do |name|
     puts "  Created #{company.jobs[num].title}"
   end
 end
+
+Job.all.each do |job|
+  num = rand(1..3)
+  num.times do
+    comment_options = [
+      Faker::Lorem.paragraphs(rand(2..3)).join("\r\n\r\n"),
+      Faker::Lorem.sentences(3).join(" "),
+      Faker::Lorem.sentences(2).join(" "),
+      Faker::Lorem.sentences(1).join(" ")
+    ]
+    job.comments.create(content: comment_options.sample)
+  end
+end
+
+position_hash = {}
+Company.all.each do |company|
+  num = rand(1..3)
+  joiner = [".","-","_",""].sample
+  num.times do
+    position = Faker::Job.title
+    position_hash[position].nil? ? position_hash[position] = 1 : position_hash[position] += 1
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    email = "#{[first_name.downcase, last_name.downcase].join(joiner)}@#{company.name.downcase}.com"
+    company.contacts.create(first_name: first_name, last_name: last_name, position: "#{position} #{position_hash[position]}", email: email)
+  end
+end
+
